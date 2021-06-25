258ddd3648185d350c85a4a08cabe90c6387ba2b
Erick Gomes <erickgm2000@hotmail.com>
2021-06-25 12:22:49 -0300

    revisão responsividade home e servicos 1

--- page-servicos.php
+++ page-servicos.php
@@ -27,7 +27,7 @@
             </p>
         </div>
         <div class="reabilitacao-credito row">
-            <span class="col-sm-12 col-xl-6">
+            <span class="col-sm-10 col-xl-6">
                 <h3><span class="blue-text">Reabilitação de crédito</span></h3>
                 <h4>Blindagem</h4>
                 <p>
@@ -37,12 +37,12 @@
                     econômica financeira ativa.
                 </p>
             </span>
-            <span class="col-sm-12 col-xl-6 image-container">
+            <span class="col-sm-2 col-xl-6 image-container">
                 <img src="<?php echo get_bloginfo('template_url') ?>/images/servicos/blindagem.svg" alt="Blindagem imagem">
             </span>
         </div>
         <div class="retirada-apontamentos row">
-            <span class="col-sm-12 col-xl-6 image-container">
+            <span class="col-sm-12 col-xl-6 image-container linha">
                 <img src="<?php echo get_bloginfo('template_url') ?>/images/servicos/linhas 1.svg" alt="Linhas">
             </span>
             <span class="col-sm-12 col-xl-6">
@@ -71,15 +71,15 @@
                     financiamentos e até mesmo na liberação do limite para cheque especial
                 </p>
             </span>
-            <span class="col-sm-12 col-xl-6 image-container">
+            <span class="col-sm-12 col-xl-6 image-container linha">
                 <img src="<?php echo get_bloginfo('template_url') ?>/images/servicos/linhas 2.svg" alt="Linhas">
             </span>
         </div>
         <div class="bacen row">
-            <span class="col-sm-12 col-xl-6 image-container">
+            <span class="col-sm-2 col-xl-6 image-container">
                 <img src="<?php echo get_bloginfo('template_url') ?>/images/servicos/bacen.svg" alt="Bacen">
             </span>
-            <span class="col-sm-12 col-xl-6">
+            <span class="col-sm-10 col-xl-6">
                 <h3><span class="blue-text">Bacen</span></h3>
                 <h4>Exclusão do Endividamento Total no Banco Central</h4>
                 <p>
