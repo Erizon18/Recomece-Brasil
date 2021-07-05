294833b51802089f84c661c1ac0b84c4328edf3f
Caio Brandao <caiobrandz@gmail.com>
2021-07-05 15:38:05 -0300

    ajuste post e quem somos

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -12,7 +12,7 @@
 
     <div class="container">
         <div class="historia row">
-            <span class="col-sm-12 col-xl-8">
+            <span class="col-sm-12 col-xl-7">
                 <div class="title">
                     <h3><span class="bold-text">Nossa</span> <span class="blue-text">História</span></h3>
                 </div>
@@ -23,37 +23,38 @@
                 toda a diferença para quem está lutando para <span class="boldblue">RECOMEÇAR.<span></h5>
                 
             </span>
-            <span class="col-sm-12 col-xl-4 image-container">
+            <span class="col-sm-12 col-xl-5 image-container">
                 <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/hist.svg" alt="historialogo"/>
             </span>
         </div>
-    
+
+
         <div id="valores row">
             <div class="title">
                 <h3><span class="bold-text">Nossos</span> <span class="blue-text">valores</span></h3>
             </div>
-            <div id="valores-caixa">
-                <div class="valor">
+            <div class="valores-caixa col-xl-12 row justify-content-center">
+                <div class="valor col-xl-3 col-12 col-md-5">
                     <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/valor1.svg" alt="historialogo"/>
                     <h4>Resolvemos problemas</h4>
                     <p>Fazemos isso da maneira mais rápida e simples possível, centralizando nossos esforços para que a experiência do cliente seja satisfatória.</p>
                 </div>
-                <div class="valor">
+                <div class="valor col-xl-3 col-12 col-md-5">
                     <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/valor2.svg" alt="historialogo"/>
                     <h4>Nosso propósito é forte</h4>
                     <p>Sabemos onde queremos chegar. Temos um propósito forte e não abrimos mão da nossa essência para encurtar caminhos.</p>
                 </div>
-                <div class="valor">
+                <div class="valor col-xl-3 col-12 col-md-5">
                     <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/valor3.svg" alt="historialogo"/>
                     <h4>Agimos com idoneidade</h4>
                     <p>Agimos com ética, moral e responsabilidade em todos os casos: Estejamos envolvidos direta ou indiretamente na situação.</p>
                 </div>
-                <div class="valor">
+                <div class="valor col-xl-3 col-12 col-md-5">
                     <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/valor4.svg" alt="historialogo"/>
                     <h4>Somos um time</h4>
                     <p>Manter os companheiros motivados e empenhados a ir mais longe é a consequência de entendermos que juntos somos mais fortes. </p>
                 </div>
-                <div class="valor">
+                <div class="valor col-xl-3  col-12 col-md-5">
                     <img src="<?php echo get_bloginfo('template_url') ?>/images/quem-somos/valor5.svg" alt="historialogo"/>
                     <h4>Entregamos o melhor</h4>
                     <p>Não apenas entregamos uma solução para o cliente, como entregamos A MELHOR solução que estiver ao nosso alcance.</p>
